2ee3907b2cd4d7820a4a4ee998171829e9b1a45e
imon-n <imon.eeecu@gmail.com>
2025-11-17 08:37:04 +0600

    added fuction

---
+++ src/function.ts
@@ -0,0 +1,33 @@
+// function
+// arrow function, nirmal function
+
+function addNormal(num1: number, num2:number): number{
+    return num1 + num2;
+}
+// add(2,"2");
+// add(2,2);
+
+const addArrow = (num1: number, num2:number): number => num1 + num2;
+
+addArrow(3,4);
+
+// object => function =>method
+const poorUser = {
+    name: "mezba",
+    balance: 0,
+    addBalance(value: number):number{
+        const totalBalance = this.balance + value;
+        return totalBalance;
+    }
+}
+
+poorUser.addBalance(100000);
+
+// map
+const arr: number[] = [1,4,6];
+const sqrArray = arr.map((elem:number):number => elem* elem)
+
+console.log(sqrArray);
+
+
+
